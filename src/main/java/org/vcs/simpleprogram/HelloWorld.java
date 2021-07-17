cddec052519c55536946baa39c4e5139bc061052
vkg7143 <74658870+vkg7143@users.noreply.github.com>
2021-07-17 22:43:23 +0530

    stash test
    (cherry picked from commit 34cd3feca41e41be26583a7adb1ddb902eec7918)
    
    # Conflicts:
    #	src/main/java/org/vcs/simpleprogram/HelloWorld.java

--- src/main/java/org/vcs/simpleprogram/HelloWorld.java
+++ src/main/java/org/vcs/simpleprogram/HelloWorld.java
@@ -5,7 +5,5 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("this is my second commit");
         System.out.println("this is direct commit from git direct");
-        System.out.println("Stash demo");
-        System.out.println("stash test");
     }
 }
