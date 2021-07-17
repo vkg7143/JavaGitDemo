34cd3feca41e41be26583a7adb1ddb902eec7918
vkg7143 <74658870+vkg7143@users.noreply.github.com>
2021-07-17 22:43:23 +0530

    stash test

--- src/main/java/org/vcs/simpleprogram/HelloWorld.java
+++ src/main/java/org/vcs/simpleprogram/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("this is my second commit");
         System.out.println("this is direct commit from git direct");
+        system.out.println("stash test");
     }
 }
