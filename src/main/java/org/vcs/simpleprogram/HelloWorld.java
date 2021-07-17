bc4a19fe30d1cd09497c3bff69901e6a8071a791
vkg7143 <74658870+vkg7143@users.noreply.github.com>
2021-07-17 22:13:40 +0530

    Update HelloWorld.java
    direct change from git

--- src/main/java/org/vcs/simpleprogram/HelloWorld.java
+++ src/main/java/org/vcs/simpleprogram/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("this is my second commit");
+        System.out.println("this is direct commit from git direct");
     }
 }
