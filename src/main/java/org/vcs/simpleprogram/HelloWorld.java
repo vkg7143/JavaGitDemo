99cf18ce1a5a30fffb5fa1b77d064597676bd574
vivek kumar <vkg7143@gmail.com>
2021-07-17 22:03:18 +0530

    second update

--- src/main/java/org/vcs/simpleprogram/HelloWorld.java
+++ src/main/java/org/vcs/simpleprogram/HelloWorld.java
@@ -3,5 +3,6 @@ package org.vcs.simpleprogram;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("this is my second commit");
     }
 }
