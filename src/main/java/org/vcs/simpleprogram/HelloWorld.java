de10b46557f1dd2cd9a61586c733fdd40b073563
vivek kumar <vkg7143@gmail.com>
2021-07-17 22:31:04 +0530

    Revert "revert test"
    This reverts commit afaac1d26a08392b42a2d8f854eb791deabaa15e.

--- src/main/java/org/vcs/simpleprogram/HelloWorld.java
+++ src/main/java/org/vcs/simpleprogram/HelloWorld.java
@@ -5,6 +5,5 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("this is my second commit");
         System.out.println("this is direct commit from git direct");
-        System.out.println("this is to check revert");
     }
 }
