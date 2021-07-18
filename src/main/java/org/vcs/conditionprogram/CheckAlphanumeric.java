75cee36156153a2df73c2bfa7c2b7c2d829895a9
vivek kumar <vkg7143@gmail.com>
2021-07-18 17:43:12 +0530

    sonar issue fixed

--- src/main/java/org/vcs/conditionprogram/CheckAlphanumeric.java
+++ src/main/java/org/vcs/conditionprogram/CheckAlphanumeric.java
@@ -1,7 +1,5 @@
 package org.vcs.conditionprogram;
 
-import javax.xml.bind.SchemaOutputResolver;
-
 public class CheckAlphanumeric {
     public static void main(String[] args) {
         char str = '9';
@@ -9,9 +7,7 @@ public class CheckAlphanumeric {
     }
 
     static void checkAlphaNumeric(char str) {
-        boolean result;
-
-        if ((str > 'a' && str < 'z') || (str > 'A' && str < 'Z')) {
+             if ((str > 'a' && str < 'z') || (str > 'A' && str < 'Z')) {
             System.out.println("character is alphabet");
         } else {
             System.out.println("character is digit");
