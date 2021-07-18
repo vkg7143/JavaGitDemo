fe6dd80bde731d8230619e7e940e40aca4651d15
vivek kumar <vkg7143@gmail.com>
2021-07-18 19:04:20 +0530

    String simple program1

---
+++ src/main/java/org/vcs/array/StringProgramSimple.java
@@ -0,0 +1,11 @@
+package org.vcs.array;
+
+import com.sun.xml.internal.ws.api.client.SelectOptimalEncodingFeature;
+
+public class StringProgramSimple {
+    public static void main(String[] args) {
+        String str="My name is khan";
+        System.out.println(str);
+    }
+
+}
